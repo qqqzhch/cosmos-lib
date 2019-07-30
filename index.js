65a81af998edb2f9dc66c30a35a38e95629cc5fe
zhangchao <544262408@qq.com>
2019-07-30 13:10:12 +0800

    index

--- index.js
+++ index.js
@@ -5,6 +5,10 @@
 
 exports.address = {};
 exports.crypto  = {};
+exports.publicKey={};
+exports.privateKey={};
 
 Object.assign(exports.address, require('./lib/address'));
 Object.assign(exports.crypto,  require('./lib/crypto'));
+Object.assign(exports.publicKey,  require('./lib/publicKey'));
+Object.assign(exports.privateKey,  require('./lib/privateKey'));
