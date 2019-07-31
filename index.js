4c19d52f9fe578d60dcc16304e5ecfe316ed551b
zhangchao <544262408@qq.com>
2019-07-31 14:45:51 +0800

    keyStore

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ exports.address = {};
 exports.crypto  = {};
 exports.publicKey={};
 exports.privateKey={};
+exports.keyStore={};
 
 Object.assign(exports.address, require('./lib/address'));
 Object.assign(exports.crypto,  require('./lib/crypto'));
 Object.assign(exports.publicKey,  require('./lib/publicKey'));
 Object.assign(exports.privateKey,  require('./lib/privateKey'));
+Object.assign(exports.keyStore,  require('./lib/keyStore'));
